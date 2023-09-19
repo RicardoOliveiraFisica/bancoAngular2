b8f180e1c7e43d8e07fe4126c03f2db75b294bc2
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-09-19 20:23:53 -0300

    criação de modulos e paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { LoansComponent } from './pages/loans/loans.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'clients', component: ClientsComponent},
+  {path: 'loans', component: LoansComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavebarComponent } from './components/navebar/navebar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { LoansComponent } from './pages/loans/loans.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavebarComponent,
+    ButtonComponent,
+    HomeComponent,
+    ClientsComponent,
+    LoansComponent
   ],
   imports: [
     BrowserModule,
